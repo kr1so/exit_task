ac0a7854c7348a19a4194ade9eb3d26d0d1c6573
kris.o <yajkakusta@gmail.com>
2021-12-28 20:26:54 +0500

    тестовое решение задачи без юзинга методов

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿int[] array1 = new int[10];
+﻿// Первый вариант:
+/* int[] array1 = new int[10];
 for (int i = 0; i < array1.Length; i++){
     array1[i] = new Random().Next(-10, 10);
     System.Console.Write($"{array1[i]} ");
@@ -12,4 +13,28 @@ for (int i = 0; i < array1.Length; i++){
         System.Console.Write($"{array2[count]} ");
         count++;
     }
+} */
+
+// Второй вариант:
+
+int[] array1 = new int[10];
+int[] FillRandomNumbers(int[] array){
+for (int i = 0; i < array.Length; i++){
+array[i] = new Random().Next(-10, 10);
 }
+return array;
+}
+System.Console.Write(FillRandomNumbers(array1));
+
+
+ //   System.Console.Write($"{array1[i]} ");
+/* System.Console.WriteLine();
+int count = 0;
+int[] array2 = new int[array1.Length];
+for (int i = 0; i < array1.Length; i++){
+    if (array1[i] % 2 == 0){
+        array2[count] = array1[i];
+        System.Console.Write($"{array2[count]} ");
+        count++;
+    }
+} */
