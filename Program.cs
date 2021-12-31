98804575b7963452d7397da52b8ffa51e7abc277
kris.o <yajkakusta@gmail.com>
2021-12-31 18:22:07 +0500

    убрала варианты решения, оставила одно с методами

--- Program.cs
+++ Program.cs
@@ -1,22 +1,4 @@
-﻿// Первый вариант:
-/* int[] array1 = new int[10];
-for (int i = 0; i < array1.Length; i++){
-    array1[i] = new Random().Next(-10, 10);
-    System.Console.Write($"{array1[i]} ");
-}
-System.Console.WriteLine();
-int count = 0;
-int[] array2 = new int[array1.Length];
-for (int i = 0; i < array1.Length; i++){
-    if (array1[i] % 2 == 0){
-        array2[count] = array1[i];
-        System.Console.Write($"{array2[count]} ");
-        count++;
-    }
-} */
-
-// Второй вариант:
-int[] RandomNumber(int[] array){
+﻿int[] RandomNumber(int[] array){
     for (int i = 0; i < array.Length; i++){
         array[i] = new Random().Next(-10, 10);
     }
